c31814056fcb4ac28bc8d814e522050fd36d4738
coldstarrrr <82551249+coldstarrrr@users.noreply.github.com>
2022-05-09 23:29:45 -0400

    updated Tweet Cleaner

--- TweetCleaner.py
+++ TweetCleaner.py
@@ -7,10 +7,14 @@ words = set(nltk.corpus.words.words())
 
 ## usage: train_corpus['text'] = train_corpus['text'].apply(preprocess)
 def preprocess(tweet):
-    tweet = re.sub("@[A-Za-z0-9]+","",tweet) #Remove @ sign
-    tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", tweet) #Remove http links
-    tweet = " ".join(tweet.split())
-    tweet = ''.join(c for c in tweet if c not in emoji.UNICODE_EMOJI) #Remove Emojis
-    tweet = tweet.replace("#", "").replace("_", " ") #Remove hashtag sign but keep the text
-    tweet = " ".join(w for w in nltk.wordpunct_tokenize(tweet) if w.lower() in words or not w.isalpha())
+    tweet = tweet.replace(r"&amp;?", r"and")
+    tweet = tweet.replace('&lt;', r'<')
+    tweet = tweet.replace('&gt;', r'>')
+    tweet = re.sub("@[A-Za-z0-9]+", "", tweet)  # Remove @ sign
+    tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", " ", tweet)  # Remove http links
+    tweet = re.sub(r"[^\w\s]", " ", tweet) # remove punctuations
+    tweet = ''.join(c for c in tweet if c not in emoji.UNICODE_EMOJI)  # Remove Emojis
+    tweet = re.sub(r" +", " ", tweet) # remove excess white spaces
+    tweet = tweet.strip() # remove white spaces at the beginning & end of the string
+
     return tweet
