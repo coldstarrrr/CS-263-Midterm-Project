c9ac0f196d0fe4c2292dc9e01772f5bbdd3d0f1e
coldstarrrr <82551249+coldstarrrr@users.noreply.github.com>
2022-05-09 23:31:48 -0400

    updated Tweet Cleaner

--- TweetCleaner.py
+++ TweetCleaner.py
@@ -10,11 +10,12 @@ def preprocess(tweet):
     tweet = tweet.replace(r"&amp;?", r"and")
     tweet = tweet.replace('&lt;', r'<')
     tweet = tweet.replace('&gt;', r'>')
-    tweet = re.sub("@[A-Za-z0-9]+", "", tweet)  # Remove @ sign
+    tweet = re.sub("@[A-Za-z0-9]+ ", "", tweet)  # Remove @ sign
     tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", " ", tweet)  # Remove http links
-    tweet = re.sub(r"[^\w\s]", " ", tweet) # remove punctuations
+    tweet = re.sub(r"[^\w\s]", " ", tweet)  # remove punctuations
     tweet = ''.join(c for c in tweet if c not in emoji.UNICODE_EMOJI)  # Remove Emojis
-    tweet = re.sub(r" +", " ", tweet) # remove excess white spaces
-    tweet = tweet.strip() # remove white spaces at the beginning & end of the string
+    tweet = re.sub(r" +", " ", tweet)  # remove excess white spaces
+    tweet = tweet.strip()  # remove white spaces at the beginning & end of the string
+    tweet = tweet.lower()
 
     return tweet
